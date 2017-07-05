11517807aec5f36db6eb931c5fd0a9478c5ade3e
Giovanni <camurati@eurecom.fr>
2017-07-05 17:16:54 +0200

    first prototype of the generator

---
+++ generator.py
@@ -0,0 +1,28 @@
+# @Author: Giovanni Camurati <Camurati>, Corteggiani Nassim <Corteggiani>
+# @Date:   04-July-2017
+# @Email:  camurati@eurecom.fr, nassim.corteggiani@maximintegrated.com
+# @Filename: generator.py
+# @Last modified by:   Camurati
+# @Last modified time: 04-July-2017
+# @License: GPLv3
+
+# Import the interactive module, responsible for the communication with the real
+# device. Set the path to the libinception cpp library that handles jtag. Use it
+# in non command line mode
+import sys
+import time
+sys.path.append('../RTDebugger-driver/src/')
+import interactive
+
+# the real device, used as golden model
+device = interactive.Interactive('../RTDebugger-driver/Debug/libinception.so',False)
+
+# execute on the real device
+device.halt()
+device.load_binary_in_sram('./main.bin',0x10000000)
+device.write_reg(15,0x10000000)
+device.write_reg(0,0)
+device.resume()
+time.sleep(0.01)
+device.halt()
+device.read_reg(0)
