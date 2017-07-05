bdef0e19b7263508b93487a72c1b24237a8ada4c
Giovanni <camurati@eurecom.fr>
2017-07-05 21:40:44 +0200

    automated generation of test code

--- generator.py
+++ generator.py
@@ -15,8 +15,29 @@ import os
 sys.path.append('../RTDebugger-driver/src/')
 import interactive
 
+# the real device, used as golden model
+device = interactive.Interactive('../RTDebugger-driver/Debug/libinception.so',False)
+
+
 # generate the test code
-# TODO
+with open('main.c',mode='wt') as main_file:
+    main_file.write("#include <stdlib.h>\n")
+    main_file.write("__attribute__((naked))\n")
+    main_file.write("void main(void){\n")
+   
+    # input operands
+    reg="r0"
+    val="#5"
+    main_file.write('  __asm volatile("mov %s,%s"); \n'%(reg,val))
+    
+    # instruction under test
+    instr="mov"
+    operands="r3,r0"
+    main_file.write('  __asm volatile("%s %s");\n'%(instr,operands))
+    
+    main_file.write("  while(1);\n")
+    main_file.write("}\n")
+main_file.close
 
 # compile the code for inception
 # TODO
@@ -24,15 +45,13 @@ import interactive
 # compile the code for the real device
 os.system('make')
 
-# the real device, used as golden model
-device = interactive.Interactive('../RTDebugger-driver/Debug/libinception.so',False)
-
 # execute on the real device
 # and dump the differencies in the values of the registers before and after
 # execution
 device.halt()
 device.load_binary_in_sram('./main.bin',0x10000000)
 device.write_reg(15,0x10000000)
+device.clear_all_regs()
 regs_initial = device.dump_all_regs()
 device.resume()
 time.sleep(0.01)
@@ -41,6 +60,7 @@ regs_final = device.dump_all_regs()
 with open('reg_diff.log',mode='wt') as reg_diff_file:
     reg_diff_file.write("test\n")
     for (initial_name,initial_val),(final_name,final_val) in zip(regs_initial.items(),regs_final.items()):
+        # print("%d %d\n"%(initial_val,final_val))
         if(final_val != initial_val):
             reg_diff_file.write("%s %d\n"%(final_name,final_val))
     reg_diff_file.close
