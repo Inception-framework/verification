5c30080b313249d85ba4f8cffce4f03cdf03a979
Giovanni <camurati@eurecom.fr>
2017-07-05 21:41:24 +0200

    main is now generated by the script

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-__attribute__((naked))
-void main(void){
-  // set input registers
-  __asm volatile("mov r0,#5");
-  // instruction under test
-  __asm volatile("mov r3,r0");
-  while(1);
-}
