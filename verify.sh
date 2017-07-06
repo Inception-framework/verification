f83763a198bf6c9494ca67ae62d72988875ae059
Giovanni <camurati@eurecom.fr>
2017-07-07 00:12:35 +0200

    error in filenames solved

--- verify.sh
+++ verify.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cd main
 
-rm registers$1.dump
+rm registers.dump
 cp ../../Samples/lpc1800-demos/config.json .
 klee -search=dfs main$1_merged.bc
 rm -rf klee-*
-cat registers$1.dump | awk 'BEGIN {for(i=1; i<=6; i++){getline;}} \
+cat registers.dump | awk 'BEGIN {for(i=1; i<=6; i++){getline;}} \
 {print $3; getline; print $2; for(i=1; i<=4; i++) \
 {getline;}}' > reg_diff_klee$1.log
 DIFFERENCE=$(diff reg_diff$1.log reg_diff_klee$1.log)
