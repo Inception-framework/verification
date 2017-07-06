2dbe70389030eebad8eb8b790f78078df3e5de94
Giovanni <camurati@eurecom.fr>
2017-07-06 18:06:40 +0200

    verify.sh generic

--- verify.sh
+++ verify.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 cd main
-rm registers.dump
+
+rm registers$1.dump
 cp ../../Samples/lpc1800-demos/config.json .
-klee -search=dfs main_merged.bc
+klee -search=dfs main$1_merged.bc
 rm -rf klee-*
-cat registers.dump | awk 'BEGIN {for(i=1; i<=6; i++){getline;}} \
+cat registers$1.dump | awk 'BEGIN {for(i=1; i<=6; i++){getline;}} \
 {print $3; getline; print $2; for(i=1; i<=4; i++) \
-{getline;}}' > reg_diff_klee.log
-DIFFERENCE=$(diff reg_diff.log reg_diff_klee.log)
+{getline;}}' > reg_diff_klee$1.log
+DIFFERENCE=$(diff reg_diff$1.log reg_diff_klee$1.log)
 if [ "$DIFFERENCE" != "" ]; then
   echo "TEST FAILED!! registers differ:"
   echo $DIFFERENCE
