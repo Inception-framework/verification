0c3fbc33b966ee7f98ab1dcd326b933fd56162c5
Giovanni <camurati@eurecom.fr>
2017-07-07 11:17:44 +0200

    module to wrap os run stuff easily

---
+++ os_run.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3.5
+# @Author: Giovanni Camurati <Camurati>, Corteggiani Nassim <Corteggiani>
+# @Date:   04-July-2017
+# @Email:  camurati@eurecom.fr, nassim.corteggiani@maximintegrated.com
+# @Filename: generator.py
+# @Last modified by:   Camurati
+# @Last modified time: 04-July-2017
+# @License: GPLv3
+
+import os
+import sys
+
+def run_catch_error(command_str, continue_stop_n):
+    ret = os.system(command_str)
+    if(ret != 0):
+        print("")
+        print("-> fail while running from python: %s"%(command_str))
+        if(continue_stop_n == False):
+            print("aborting...")
+            sys.exit(1)
+        else:
+            print("continuing anyway...")
+        print("")
+        
+         
+           
