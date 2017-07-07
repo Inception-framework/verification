93bb410156ffb10d665e6a76f96575f3d2d6716a
Corteggiani <nassim.corteggiani@maximintegrated.com>
2017-07-07 09:32:09 +0200

    More output and check commands returns

--- run_klee.sh
+++ run_klee.sh
@@ -1,25 +1,73 @@
 #!/bin/sh
-cd main
 
-for i in $(seq 0 $(expr $(cat Ntests) - 1)); do 
+if [ "$#" != 1 ]; then
+  echo "Missing arguments ..."
+  echo "$0 <ITERATION_NUMBER>"
+  exit
+fi
 
-  rm registers.dump
+run_klee() {
+
+  arg1=$1
+
+  # Remove old dump file
+  rm registers.dump >&-
+
+  klee -search=dfs main${arg1}_merged.bc  >&-
+
+  # Load the configuration file
   cp ../../Samples/lpc1800-demos/config.json .
-  klee -search=dfs main${i}_merged.bc
-  rm -rf klee-*
+
+  # Remove old log
+  rm -rf klee-* >&-
+
+  # Run klee
+  klee -search=dfs main${arg1}_merged.bc  >&-
+  if [ $? != 0 ]; then
+          printf "---> failed"
+          exit 1;
+  fi
+
+  # Backup dump files
+  cp registers.dump ${arg1}.klee.dump
+  if [ $? != 0 ]; then
+          printf "Klee encountered an error ...\n"
+          exit 1;
+  fi
+}
+
+extract_dump_info() {
+
   cat registers.dump | awk 'BEGIN {for(i=1; i<=6; i++){getline;}} \
+
   {print $3; getline; print $2; for(i=1; i<=4; i++) \
-  {getline;}}' > reg_diff_klee$i.log
-  #DIFFERENCE=$(diff reg_diff$i.log reg_diff_klee$i.log)
-  #if [ "$DIFFERENCE" != "" ]; then
-  #  echo "TEST FAILED!! registers differ:"
-  #  echo $DIFFERENCE
-  #  cd ..
-  #  exit 1
-  #fi 
-  #echo "TEST PASSED"
 
-done
+  {getline;}}' > reg_diff_klee$1.log
+}
 
-cd ..
-exit 0
+main() {
+
+  echo "-----------------[KLEE]------------------------"
+
+  #Source code directory
+  cd main
+
+  #For each test run
+  for i in `seq 0 $1`;
+  do
+    echo "[RUNNING]\t Test ${i} is running ..."
+    run_klee ${i}
+    echo "Done"
+
+    echo "[PARSER]\t Parsing dump ${i} ..."
+    extract_dump_info $i
+    echo "Done"
+  done
+
+  cd ..
+
+  echo "-----------------------------------------------"
+  exit 0
+}
+
+main $1
--- verify.py
+++ verify.py
@@ -10,20 +10,26 @@
 import os
 import sys
 
-os.system("./run_klee.sh")
-
+# Retrieve number of generated tests
 with open('main/Ntests',mode='r') as Ntests_file:
     Ntests = int(Ntests_file.readline())
 Ntests_file.close
 
+# Run klee and dump registers
+print ("Running klee ...")
+ret = os.system("./run_klee.sh "+str(Ntests))
+if ret != 0 :
+    print ("-> fail")
+    sys.exit()
 
 for i in range(0,Ntests):
+
     with open('main/reg_diff%d.log'%(i),mode='r') as reg_diff_file:
         lines = reg_diff_file.read().splitlines()
     reg_diff_file.close
     reg_diff = set([(reg,val) for reg,val in zip(lines[::2],lines[1::2])])
     #print (reg_diff)
-    
+
     with open('main/reg_diff_klee%d.log'%(i),mode='r') as reg_diff_klee_file:
         lines_klee = reg_diff_klee_file.read().splitlines()
     reg_diff_klee_file.close
@@ -34,14 +40,20 @@ for i in range(0,Ntests):
     if(diff == set()):
         print ("TEST %d PASSED"%(i))
     else:
-        print ("TEST %d FAILED"%(i))
-        print("reg_diff_klee",reg_diff_klee)
-        print("reg_diff     ",reg_diff)
+        print ("-----------------[TEST %d]---------------------"%(i))
+        print ("[RESULT]\t The test failed ...")
+        print ("")
+        print ("[Source Code]\t Available into main/main%d.c :"%(i))
+        print ("")
+        with open('main/main%d.c'%(i),mode='r') as source_file:
+            print (source_file.read())
+        print ("")
+        print ("\t[Oracle]")
+        print ("Register names and values ??")
+        print ("\t[Result]")
+        print ("Register names and values ??")
+        print ("reg_diff_klee",reg_diff_klee)
+        print ("reg_diff     ",reg_diff)
         print ("klee - device",diff)
-    print("")
-
-
-       
-
-
-
+        print ("-----------------------------------------------")
+        print("")
