c4936edad08709c35dae7b52d9adbdde9772c1e4
Giovanni <camurati@eurecom.fr>
2017-07-19 00:50:24 +0200

    filter on compared regs

--- verify.py
+++ verify.py
@@ -15,19 +15,21 @@ import os_run
 # read command line args
 def print_usage_error():
     print("Wrong parameters, usage:")
-    print("./generator.py \
+    print("./verify.py \
            -c <(continue in case of error) True/False> \
            [-i <input folder> \
-           [-o <output folder>")
+           [-o <output folder> \
+           [-f <reglist to filter>]")
     sys.exit(0)
 
 ifolder="test_cases"
 ofolder="test_results"
+filter_reglist = []
 
 if len(sys.argv) <= 1:
     print_usage_error()
 try:
-    opts,args = getopt.getopt(sys.argv[1:],"hc:i:o:",["help",
+    opts,args = getopt.getopt(sys.argv[1:],"hc:i:o:f:",["help",
                                                        "continue=",
                                                        "input-folder=",
                                                        "output-folder="])
@@ -40,6 +42,8 @@ for opt,arg in opts:
         print("    c,continue=:        True->skip errors, False->stop on error")
         print("    i,input-folder=:    folder where test cases are stored")
         print("    o,output-folder=:   folder where test results are stored")
+        print("    f,filter-reglist=:  comma-separated list of registers not \
+               to take into cosideration for the check")
         print("")
     elif opt in ("-c","--continue"):
         if arg == "True":
@@ -53,6 +57,8 @@ for opt,arg in opts:
             ifolder = arg 
     elif opt in ("-o","--output-folder="):
             ofolder = arg 
+    elif opt in ("-f","--filter-reglist="):
+            filter_reglist = arg.split(",")
 
 # create output folder starting from the input
 os_run.run_catch_error("mkdir -p %s"%(ofolder),False)
@@ -86,13 +92,17 @@ for i in range(0,Ntests):
     with open('%s/reg_diff%d.log'%(ofolder,i),mode='r') as reg_diff_file:
         lines = reg_diff_file.read().splitlines()
     reg_diff_file.close
-    reg_diff = set([(reg,val) for reg,val in zip(lines[::2],lines[1::2])])
+    reg_diff = set([(reg,val) \
+                   for reg,val in zip(lines[::2],lines[1::2]) \
+                   if reg not in filter_reglist])
     #print (reg_diff)
 
     with open('%s/reg_diff_klee%d.log'%(ofolder,i),mode='r') as reg_diff_klee_file:
         lines_klee = reg_diff_klee_file.read().splitlines()
     reg_diff_klee_file.close
-    reg_diff_klee = set([(reg,val) for reg,val in zip(lines_klee[::2],lines_klee[1::2])])
+    reg_diff_klee = set([(reg,val) \
+                        for reg,val in zip(lines_klee[::2],lines_klee[1::2]) \
+                        if reg not in filter_reglist])
     #print (reg_diff_klee)
 
     diff = reg_diff_klee.symmetric_difference(reg_diff)
