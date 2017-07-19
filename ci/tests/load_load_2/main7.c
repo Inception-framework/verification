fce06409b702b1eae28a3580f7529cdb7c0d4daf
Giovanni <camurati@eurecom.fr>
2017-07-19 22:48:36 +0200

    better tests

---
+++ ci/tests/load_load_2/main7.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+__attribute__((naked))
+void main(void){
+  __asm volatile("mov R1,#0x6c"); 
+  __asm volatile("mov R5,#0x51"); 
+  __asm volatile("mov R10,#0xc9"); 
+  __asm volatile("mov R11,#0xe3"); 
+  __asm volatile("mov R12,#0x12"); 
+  __asm volatile("mov R14,#0xba"); 
+  __asm volatile("LDRB R1, [R2 ,#-8]!");
+  __asm volatile("mov R1,#0x12"); 
+  __asm volatile("mov R5,#0xdc"); 
+  __asm volatile("mov R10,#0xbe"); 
+  __asm volatile("mov R11,#0x89"); 
+  __asm volatile("mov R12,#0x0e"); 
+  __asm volatile("mov R14,#0xee"); 
+  __asm volatile("LDRB R1, [R2 ,#-8]!");
+  #ifndef KLEE
+  while(1);
+  #else
+  __asm volatile("bx lr");
+  #endif
+}
