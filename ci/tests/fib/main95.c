9c7f0b524067a50c6c999c3c967256ec76dd2e2f
Giovanni <camurati@eurecom.fr>
2017-08-18 10:38:49 +0200

    100 working fibonacci tests

--- ci/tests/fib/main95.c
+++ ci/tests/fib/main95.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 __attribute__((naked))
 void main(void){
-  __asm volatile("mov	R0,#0"); 
+  __asm volatile("mov	R0,#4"); 
   __asm volatile("push	{R7}"); 
   __asm volatile("sub	sp, #28"); 
   __asm volatile("add	R7, sp, #0"); 
@@ -21,9 +21,9 @@ void main(void){
   __asm volatile("str	R3, [R7, #12]"); 
   __asm volatile("b.n	fibonaccix32"); 
   __asm volatile("fibonaccix22:"); 
-  __asm volatile("ldr	R2, [R7, #20]"); 
+  __asm volatile("ldr	R11, [R7, #20]"); 
   __asm volatile("ldr	R3, [R7, #16]"); 
-  __asm volatile("add	R3, R2"); 
+  __asm volatile("add	R3, R11"); 
   __asm volatile("str	R3, [R7, #12]"); 
   __asm volatile("ldr	R3, [R7, #16]"); 
   __asm volatile("str	R3, [R7, #20]"); 
@@ -34,9 +34,9 @@ void main(void){
   __asm volatile("adds	R3, #1"); 
   __asm volatile("str	R3, [R7, #8]"); 
   __asm volatile("fibonaccix38:"); 
-  __asm volatile("ldr	R2, [R7, #8]"); 
+  __asm volatile("ldr	R11, [R7, #8]"); 
   __asm volatile("ldr	R3, [R7, #4]"); 
-  __asm volatile("cmp	R2, R3"); 
+  __asm volatile("cmp	R11, R3"); 
   __asm volatile("blt.n	fibonaccix16"); 
   __asm volatile("ldr	R3, [R7, #12]"); 
   __asm volatile("mov	R0, R3"); 
