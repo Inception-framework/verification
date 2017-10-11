ffc027650e67ea2f3f65fab2a50f9f16ee34f475
Giovanni <camurati@eurecom.fr>
2017-10-11 17:51:33 +0200

    tests for add pc imm and adr

---
+++ ci/tests/addpc/main830.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+__attribute__((naked))
+void main(void){
+  __asm volatile("mov r0,#0"); 
+  __asm volatile("adds r0,r0,#0xab"); 
+  __asm volatile("mov r0,#4"); 
+  __asm volatile("lsrs r0,#1"); 
+  __asm volatile("ADD R0, R15, #0xd8");
+  #ifndef KLEE
+  while(1);
+  #else
+  __asm volatile("bx lr");
+  #endif
+}
