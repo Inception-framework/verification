bc54198dfca26a0ed44aae78bf29a120e8c4928d
Giovanni Camurati <camurati@eurecom.fr>
2017-12-08 11:29:11 +0100

    add Sample submodules

--- versioning.sh
+++ versioning.sh
@@ -14,5 +14,7 @@ echo "Inception" | tee -a $vfile
 git branch | tee -a $vfile
 git log -1 --format="%H" | tee -a $vfile
 git submodule status | tee -a $vfile
+cd Samples
+git submodule status | tee -a $vfile
 cd $DIR
 
